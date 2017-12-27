4e4a076aacc8db620ca2ec92dd628da5cb48e0eb
adam <adam@lightboxarchitecture.com.au>
2017-12-27 11:50:48 +1000

    Install gulp

--- DutchTreat/wwwroot/js/index.js
+++ DutchTreat/wwwroot/js/index.js
@@ -1,24 +1,24 @@
 ﻿$(document).ready(function () {
-    console.log("Hello Pluralsight");
+    console.log('Hello Pluralsight');
 
-    var theForm = $("#theForm");
+    var theForm = $('#theForm');
     theForm.hide();
 
-    var button = $("#buyButton");
-    button.on("click",
+    var button = $('#buyButton');
+    button.on('click',
         function () {
-            console.log("Buying an item");
+            console.log('Buying an item');
         });
 
-    var productInfo = $(".product-props li");
-    productInfo.on("click", function () {
-        console.log(`You clikced on ${$(this).text()}`);
+    var productInfo = $('.product-props li');
+    productInfo.on('click', function () {
+        console.log('You clikced on' + $(this).text());
     });
 
-    var $loginToggle = $("#loginToggle");
-    var $popupForm = $(".popup-form");
+    var $loginToggle = $('#loginToggle');
+    var $popupForm = $('.popup-form');
 
-    $loginToggle.on("click", function() {
+    $loginToggle.on('click', function() {
         $popupForm.fadeToggle(1000);
     });
 });
